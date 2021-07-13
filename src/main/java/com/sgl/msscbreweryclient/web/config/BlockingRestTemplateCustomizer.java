b1098ef0822377dc23e70bf1c8d9949e7ee56af0
lokhansn@gmail.com <lokhansn@gmail.com>
2021-07-13 16:32:38 +0530

    Adding custom Apache HTTP Client

---
+++ src/main/java/com/sgl/msscbreweryclient/web/config/BlockingRestTemplateCustomizer.java
@@ -0,0 +1,45 @@
+package com.sgl.msscbreweryclient.web.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.boot.web.client.RestTemplateCustomizer;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Component
+public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+
+    public ClientHttpRequestFactory clientHttpRequestFactory() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+
+        RequestConfig requestConfig = RequestConfig
+                .custom()
+                .setConnectionRequestTimeout(3000)
+                .setSocketTimeout(3000)
+                .build();
+
+        CloseableHttpClient httpClient = HttpClients
+                .custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+
+        return new HttpComponentsClientHttpRequestFactory(httpClient);
+    }
+
+    @Override
+    public void customize(RestTemplate restTemplate) {
+        log.info("Adding custom Apache HTTP Client");
+        restTemplate.setRequestFactory(this.clientHttpRequestFactory());
+    }
+}
